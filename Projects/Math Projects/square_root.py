ec3f69a3f52d9d4af688325bab27842497b6214a
Alexus Huang <alexushu2325@gmail.com>
2024-02-21 21:20:02 -0800

    Updated comment on line 1 in square_root.py

--- Projects/Math Projects/square_root.py
+++ Projects/Math Projects/square_root.py
@@ -1,4 +1,4 @@
-# This file asks the user for the number they want to square root and returns the square rooted answer
+# This file asks the user for the number they want to square root and returns the square rooted answer in the terminal
 import math
 question = float(input("Enter in a number you want to square root:"))
 print(math.sqrt(question))
