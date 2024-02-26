a47d54c1aaaf1d86cefe80db5905fdd31cb3e8d7
Alexus Huang <alexushu2325@gmail.com>
2024-02-25 20:21:57 -0800

    Created word_frequency_counter.py and gave a brief description of the project

---
+++ Projects/word_frequency_counter.py
@@ -0,0 +1,5 @@
+"""
+This is a simple project that uses dictionaries to store words (key) and frequencies (values)
+The program reads text thats received from the user's input and counts the frequencies of each word
+AFter counting the words, it will print out the words and their frequencies
+"""
