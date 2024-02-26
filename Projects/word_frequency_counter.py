37706b5608fc5a392d98a711e37404c0ea5740c0
Alexus Huang <alexushu2325@gmail.com>
2024-02-25 21:56:43 -0800

    Wrote the main functions of the project. Still need to work on the word counter

--- Projects/word_frequency_counter.py
+++ Projects/word_frequency_counter.py
@@ -1,5 +1,14 @@
 """
 This is a simple project that uses dictionaries to store words (key) and frequencies (values)
 The program reads text thats received from the user's input and counts the frequencies of each word
-AFter counting the words, it will print out the words and their frequencies
+After counting the words, it will print out the words and their frequencies
 """
+word_frequencies ={} # empty dictionary
+user_text = input("Enter in some text to see how many repeated words there are:")
+word_count = 0
+
+split_words=user_text.split()  # split each word from the user's input and put it in a list
+for every_word in split_words:
+    print(every_word.count(every_word))
+    repeated_words=dict({every_word:word_count})
+    print(repeated_words)
