1b2bb89daa4c948d5140887b0526a5979112af11
Alexus Huang <alexushu2325@gmail.com>
2024-02-28 14:29:27 -0800

    Commented out previous code, and will start to rewrite everything with a fresh mind to see if the issue can be solved

--- Projects/word_frequency_counter.py
+++ Projects/word_frequency_counter.py
@@ -3,17 +3,22 @@ This is a simple project that uses dictionaries to store words (key) and frequen
 The program reads text thats received from the user's input and counts the frequencies of each word
 After counting the words, it will print out the words and their frequencies
 """
-word_frequencies ={} # empty dictionary
-user_text = input("Enter in some text to see how many repeated words there are:")
-word_count = 0
+# word_frequencies ={} # empty dictionary
+# user_text = input("Enter in some text to see how many repeated words there are:")
+# word_count = 0
 
-split_words=user_text.split()  # split each word from the user's input and put it in a list
-for every_word in split_words:  # go through every word in the list to see if there are any repeated words 
-    count_words = every_word.count(every_word)
-    if every_word != 1: # this conditional statement sees if the word repeats or doesn't repeat.The problem is here since the word count will always be one
-        word_count+=1
-    else:
-        word_count=1
-    repeated_words=dict({every_word:count_words})
-    word_frequencies.update(repeated_words)
-print(word_frequencies)
+# split_words=user_text.split()  # split each word from the user's input and put it in a list
+# for every_word in split_words:  # go through every word in the list to see if there are any repeated words 
+#     count_words = every_word.count("some") # fix this tmrw
+#     print(count_words)
+#     if every_word != 1: # this conditional statement sees if the word repeats or doesn't repeat.The problem is here since the word count will always be one
+#         word_count+=1
+#     else:
+#         word_count=1
+#     repeated_words=dict({every_word:count_words})
+#     word_frequencies.update(repeated_words)
+# print(word_frequencies)
+
+
+
+# Rewriting everything to see if the problem can be solved
