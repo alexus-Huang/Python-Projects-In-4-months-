5e9df318f881b85f45bb7f9a5efc7403ef6f5426
Alexus Huang <alexushu2325@gmail.com>
2024-02-28 18:05:47 -0800

    Completed student_grade_tracker project

--- Projects/Student Grade Tracker Project/student_grade_tracker.py
+++ Projects/Student Grade Tracker Project/student_grade_tracker.py
@@ -6,6 +6,33 @@ The user can update a student's name/grade.
 The user can find the average of their class.
 
 """
+def find_class_average(): # Print the class average to the user
+    all_scores=class_score.values()
+    add_all_scores = sum(all_scores)
+    number_of_students = len(all_scores)
+    average_score = add_all_scores/number_of_students
+    print(average_score)
+
+def remove_student():
+    student_name=input("What's the student's name?")
+    class_score.pop(student_name)
+    print(class_score)
+
+def update_list(): # The user can update a student's name and grade
+    student_name=input("What's the student's name?")
+    change_what=input("What change do you want to make? (name or grade)")
+    if change_what == "name":
+        new_name = input("What's the student's new name?") 
+        class_score[new_name]=class_score.pop(student_name)  # This is something I found on Stack Overflow. I learned that this was a simple way of switching out keys in dictionaries. This code removes the old name and assigns the dictionary key with new_name.
+    else:
+        new_grade=float(input("What is the student's new grade?"))
+        class_score[student_name]=new_grade
+    print(class_score)
+
+
+def quit_program():
+    quit
+
 class_score={}
 how_many_students=int(input("How many students are you grading?"))
 for every_student in range(how_many_students):
@@ -14,8 +41,24 @@ for every_student in range(how_many_students):
     user_input_student_grade=float(input("Student's updated grade:"))
     class_score.update({user_input_name:user_input_student_grade})
 
-def find_class_average():
-    all_scores=class_score.values()
-    print(all_scores)
 print(class_score)
-print(find_class_average())
+
+# Ask the user questions before the program quits
+ask_average=input("Would you like the class average? (yes/no)")
+if ask_average == "yes":
+    find_class_average()
+else:
+    pass
+ask_remove_student=input("Would you like to remove a student from the list? (yes/no)")
+if ask_remove_student == "yes":
+    remove_student()
+else:
+    pass
+ask_update=input("Would you like to update either a student's name or grade?")
+if ask_update == "yes":
+        update_list()
+else:
+    pass
+ask_print_again=input("The program will automatically quit. Before doing so, would you like to see the final list?")
+if ask_print_again == "yes":
+    print("Here is the final list of your student's grades:",class_score)
