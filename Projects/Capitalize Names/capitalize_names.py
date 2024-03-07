561f33c5dca8224055796bd7f633c26bcd9f5b1f
Alexus Huang <alexushu2325@gmail.com>
2024-03-06 22:47:47 -0800

    Issue on line 10 - Will fix tomorrow

--- Projects/Capitalize Names/capitalize_names.py
+++ Projects/Capitalize Names/capitalize_names.py
@@ -7,14 +7,17 @@ Ask user if they want to add names to a list to capitalize the first name
  - The function that prints out the names should use kwargs to sort out the values when printing
 """
 def capitalize_first_letter(**kwargs):
+    print(kwargs) # figure out the problem here - are lists not accepted for kwargs?
+
+
+list_of_first_names = []
+enter_name_question = input("Do you want to enter first names? (yes/no) ")
+
+if enter_name_question.lower() == "yes":
     how_many_times=int(input("How many first names do you want to enter? "))
     for i in range(how_many_times):
-        list_of_first_names = list(kwargs)
-        print(list_of_first_names)
-
-
-enter_name_question = input("Do you want to enter first names? (yes/no) ")
-if enter_name_question.lower() == "yes":
-    capitalize_first_letter()
+        user_input=input("Enter in their first name:")
+        list_of_first_names.append(user_input)
+    capitalize_first_letter(list_of_first_names)
 else:
     quit()
